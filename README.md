7370ee0ab812caf8d0948b2036cd5769b48d81c9
Lauro Becker <lauro.becker@gmail.com>
2016-09-05 15:03:07 -0300

    :tada: Very first commit, with exit intent popup. \o/ :tada:

---
+++ README.md
@@ -0,0 +1,50 @@
+# Conversion Modal
+
+This project aims to create a very easy way to open modals based on some events.
+
+The inspiration for this project is creating modal windows that increase conversion rates.
+
+## Installation
+
+This lib requires some dependencies:
+
+* [jQuery](http://jquery.com/download/);
+* [Ouibounce](https://github.com/carlsednaoui/ouibounce): For exit intent popups.
+
+TODO
+
+## Usage
+
+Here you can see usage types. Go on, pick one!
+
+## Exit Intent
+
+This type is fired when the user is leaving the page. We're using the awesome lib [Ouibounce](https://github.com/carlsednaoui/ouibounce) that abstracts the hard work.
+
+![Image of Exit Intent Modal](http://g.recordit.co/BNqTRgAiJk.gif)
+
+* [Example Here](examples/exit-intent.html)
+
+The magick is here:
+
+```javascript
+  conversionModal("<h2>Please, dont go!</h2><p>I've got something to tell you...</p>", {
+    type: 'exit_intent',
+    background: 'orange',
+    accept_text: 'Really? Tell me more...',
+    accept_link: 'http://www.organicadigital.com',
+    accept_link_target: '_blank',
+    reject_text: 'I dont care.',
+    // You can use any Ouibounce arg here
+    aggressive: true // Don't use this in production... It will show this modal EVERY time user tries to close the window.
+  });
+```
+
+
+## Options
+
+TODO Table with all options
+
+## Thanks
+
+    Made with <3 and a lot of coffee [@Orgânica Digital Innovation](http://www.organicadigital.com).
---
+++ source/conversion-modal.js
@@ -0,0 +1,61 @@
+function conversionModal(html, custom_config) {
+  'use strict';
+
+  var config = custom_config || {}
+    , type = config.type || 'exit_intent'
+    , background = config.background || 'white'
+    , accept_text = config.accept_text
+    , accept_link = config.accept_link || '#'
+    , accept_link_target = config.accept_link_target || '_self'
+    , reject_text = config.reject_text || 'No, thanks.'
+    ;
+
+  function randId() {
+    var S4 = function() {
+       return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+    };
+    return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4())
+  }
+
+  function applyStyles(modal) {
+    modal.css({background: background});
+  }
+
+  function dismiss(modal) {
+    modal.hide();
+  }
+
+  function createWrapper() {
+    var id = randId();
+    var wrapper = "<div class='conversion-modal " + type + "' id='" + 
+        id + "'><div class='conversion-modal-wrapper'>" + html + 
+        "<div class='actions'></div></div></div>";
+    document.write(wrapper);
+
+    wrapper = $(document.getElementById(id));
+
+    var actions = $(".actions", wrapper);
+    if(accept_text != undefined) {
+      actions.append("<a class='accept-link' href='" + accept_link + 
+        "' target='" + accept_link_target + "'>" + 
+        accept_text + "</a>");
+    }
+
+    actions.append("<a class='reject-link' href='#'>" + 
+        reject_text + "</a>");
+
+    $(".reject-link, .accept-link", actions).bind("click", function() {
+      dismiss(wrapper);
+    });
+
+    applyStyles(wrapper);
+    return wrapper;
+  }
+
+  function exit_intent() {
+    ouibounce(createWrapper()[0], config);
+  }
+
+  // Calls the appropriated function
+  eval(type + '();')
+};
